1eb2cc497c17aa3e8cc68189d039b8f177dafb18
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-09-15 17:16:34 -0500

    stats

--- extract-stats.py
+++ extract-stats.py
@@ -7,7 +7,6 @@ import pandas
 import scipy
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--tiv_txt', default='INPUTS/cat12/TIV/TIV.txt')
 parser.add_argument('--cat_t1_mat', default='INPUTS/cat12/MAT/cat_t1.mat')
 parser.add_argument('--catROI_t1_mat', default='INPUTS/cat12/LABEL/catROI_t1.mat')
 parser.add_argument('--catROIs_t1_mat', default='INPUTS/cat12/LABEL/catROIs_t1.mat')
@@ -49,11 +48,65 @@ def mat_walk(obj, path=""):
     yield (path, obj)
 
 
-tiv = pandas.read_csv(args.tiv_txt, sep='\t', header=None, names=['TIV','GM','WM','CSF','ICVfrac'])
-tiv.to_csv(os.path.join(args.out_dir, 'tiv.csv'), index=False)
+# Load stats results
+info = {
+    'cat_t1': scipy.io.loadmat(args.cat_t1_mat, squeeze_me=True, struct_as_record=False),
+    'catROI_t1': scipy.io.loadmat(args.catROI_t1_mat, squeeze_me=True, struct_as_record=False),
+    'catROIs_t1': scipy.io.loadmat(args.catROIs_t1_mat, squeeze_me=True, struct_as_record=False),
+    }
 
+# Save text version of complete outputs to file
 for stem in ['cat_t1', 'catROI_t1', 'catROIs_t1']:
-    info = scipy.io.loadmat(getattr(args,f'{stem}_mat'), squeeze_me=True, struct_as_record=False)
     with open(os.path.join(args.out_dir, f'{stem}.txt'), 'w', encoding='utf-8') as f:
-        for p, v in mat_walk(info):
+        for p, v in mat_walk(info[stem]):
             f.write(f'{p} = {v}\n')
+
+# Custom csv for redcap with QC info
+stats = {
+    'vol_abs_CSF_cm3': info['cat_t1']['S'].subjectmeasures.vol_abs_CGW[0],
+    'vol_abs_GM_cm3': info['cat_t1']['S'].subjectmeasures.vol_abs_CGW[1],
+    'vol_abs_WM_cm3': info['cat_t1']['S'].subjectmeasures.vol_abs_CGW[2],
+    'vol_rel_CSF': info['cat_t1']['S'].subjectmeasures.vol_rel_CGW[0],
+    'vol_rel_GM': info['cat_t1']['S'].subjectmeasures.vol_rel_CGW[1],
+    'vol_rel_WM': info['cat_t1']['S'].subjectmeasures.vol_rel_CGW[2],
+    'vol_TIV_cm3': info['cat_t1']['S'].subjectmeasures.vol_TIV,
+    'dist_thickness_mm': info['cat_t1']['S'].subjectmeasures.dist_thickness[0],
+    'dist_thickness_std': info['cat_t1']['S'].subjectmeasures.dist_thickness[1],
+    'res_RMS_abs_mm': info['cat_t1']['S'].qualitymeasures.res_RMS,
+    'NCR_abs': info['cat_t1']['S'].qualitymeasures.NCR,
+    'ICR_abs': info['cat_t1']['S'].qualitymeasures.ICR,    
+    'res_RMS_rel': info['cat_t1']['S'].qualityratings.res_RMS,
+    'NCR_rel': info['cat_t1']['S'].qualityratings.NCR,
+    'ICR_rel': info['cat_t1']['S'].qualityratings.ICR,
+    'IQR': info['cat_t1']['S'].qualityratings.IQR,
+    'EC_abs': info['cat_t1']['S'].subjectmeasures.EC_abs,
+    'defect_size': info['cat_t1']['S'].subjectmeasures.defect_size,
+    }
+
+# Compute percentage and grade letter ratings
+# https://github.com/ChristianGaser/cat12/blob/5d87f33a5cccd9a52a4fa54cd499d77fd77b5e75/cat_main_reportcmd.m#L24
+# Code conversion courtesy ChatGPT-5.2
+grades = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'E+', 'E', 'E-', 'F'];
+ngrades = len(grades)
+for var in ['res_RMS_rel', 'NCR_rel', 'ICR_rel', 'IQR']:
+    mark = numpy.asarray(stats[var], dtype=float)
+    rps = numpy.clip(105 - mark*10, 0, 100)
+    rps = rps + numpy.isnan(mark) * mark
+    stats[f'{var}_rps'] = rps
+
+    if numpy.isnan(mark):
+        idx_1based = ngrades
+    else:
+        idx_1based = int(numpy.round((mark + 2/3) * 3 - 3))
+        idx_1based = max(1, idx_1based)
+    idx_1based = min(ngrades, idx_1based)
+    grad = grades[idx_1based - 1]
+    stats[f'{var}_grade'] = grad
+
+# Save stats to file
+dstats = pandas.DataFrame([stats])
+dstats = dstats.sort_index(axis=1)
+dstats.to_csv(os.path.join(args.out_dir, 'stats.csv'), index=False)
+
+
+# FIXME Extract ROI, ROIs stats to their own files
